e61d79d60480a58d64bcb1afa4e3d94db34c00b7
unknown <AmitJindal@princenagaroutlook.onmicrosoft.com>
2025-07-08 17:45:52 +0530

    added main

---
+++ resource_group/resource.tf
@@ -0,0 +1,26 @@
+#i m from jsr
+resource "azurerm_resource_group" "rg1" {
+  name     = "app-rg-1"
+  location = "East US"
+}
+
+
+resource "azurerm_resource_group" "rg2" {
+  name     = "app-rg-2"
+  location = "West US"
+}
+
+resource "azurerm_resource_group" "rg3" {
+  name     = "app-rg-3"
+  location = "Central India"
+}
+
+resource "azurerm_resource_group" "rg4" {
+  name     = "app-rg-4"
+  location = "Central India"
+}
+
+resource "azurerm_resource_group" "rg-ram" {
+  name     = "app-rg-ram"
+  location = "Central India"
+}
